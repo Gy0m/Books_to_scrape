e2f5825ab0d162b41b06c6d59e5531fd9fe02ac9
Guillaume <landreaug@gmail.com>
2021-03-18 15:49:37 +0100

    Script can now scrape all links in a page

---
+++ Scrape_one_book.py
@@ -0,0 +1,16 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = 'http://books.toscrape.com/index.html'
+
+response = requests.get(url)
+
+if response.ok:
+    links = []
+    soup = BeautifulSoup(response.text, 'lxml')
+    divs = soup.findAll('div', class_="image_container")
+    for div in divs:
+        a = div.find('a')
+        link = a['href']
+        links.append('http://books.toscrape.com/' + link)
+    print(links)
