5c001401558d483555705b888ee700de5b92fe00
Gy0m <landreaug@gmail.com>
2021-03-22 14:45:51 +0100

    Script cann add product information in a csv file

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Books_to_scrap
+
+A little scrip to scrape imformations about books in the website 'http://books.toscrape.com/index.html'
--- Scrape_one_book.py
+++ Scrape_one_book.py
@@ -1,5 +1,9 @@
 import requests
+import ipdb
+import pandas as pd
 from bs4 import BeautifulSoup
+
+'''
 
 url = 'http://books.toscrape.com/index.html'
 
@@ -18,3 +22,39 @@ if response.ok:
 with open('urls.csv', 'w') as file:
     for link in links:
         file.write(link + '\n')
+
+with open('urls.csv', 'r') as file:
+    for row in file:
+        url = row.strip()
+        response = requests.get(url)
+        if response.ok:
+            soup = BeautifulSoup(response.text, 'lxml')
+            upc = soup.find()
+            
+        '''
+
+url = 'http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html'
+response = requests.get(url)
+if response.ok:
+    soup = BeautifulSoup(response.text, 'lxml')
+    title = soup.find('div', {'class': 'product_main'}).find('h1')
+    data_table = soup.find('table', {'class': 'table table-striped'})
+    data_table_ths =data_table('th')
+    data_table_tds = data_table.find_all('td')
+    print('Titre: ' + title.text )
+
+
+ths = []
+
+for i, th in enumerate(data_table_ths):
+    ths.append(th.text)
+
+tds = []
+
+for i, td in enumerate(data_table_tds):
+    tds.append(td.text)
+
+df_productInformation = pd.DataFrame(data={'intitulé': ths, 'informations': tds})
+print(df_productInformation)
+
+df_productInformation.to_csv('./output.csv', encoding='utf-8', index=False)
