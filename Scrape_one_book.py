c7a61dc6beacac4b02c709e233be48fbfe47a76f
Gy0m <landreaug@gmail.com>
2021-03-22 20:47:07 +0100

    add comments

--- Scrape_one_book.py
+++ Scrape_one_book.py
@@ -3,8 +3,6 @@ import ipdb
 import pandas as pd
 from bs4 import BeautifulSoup
 
-
-
 url = 'http://books.toscrape.com/index.html'
 
 response = requests.get(url)
@@ -16,38 +14,40 @@ if response.ok:
     for div in divs:
         a = div.find('a')
         link = a['href']
-        links.append('http://books.toscrape.com/' + link)
-    print(len(links))
+        links.append('http://books.toscrape.com/' + link)  #récupère les liens des images
+    print(len(links))              #compte le nombre de liens
 
 with open('urls.csv', 'w') as file:
     for link in links:
-        file.write(link + '\n')
+        file.write(link + '\n')     #copie les liens dans un fichier csv et retour à la ligne
 
 with open('urls.csv', 'r') as file:
-    for row in file:
+    with open('product_information.csv', 'w') as outf:
+        outf.write('intitulé,informations\n')
+        for row in file:
 
-        url = row.strip()
-        response = requests.get(url)
-        if response.ok:
-            soup = BeautifulSoup(response.text, 'lxml')
-            title = soup.find('div', {'class': 'product_main'}).find('h1')
-            data_table = soup.find('table', {'class': 'table table-striped'})
-            data_table_ths =data_table('th')
-            data_table_tds = data_table.find_all('td')
-            print('Titre: ' + title.text )
+            url = row.strip()
+            response = requests.get(url)
+            if response.ok:
+                soup = BeautifulSoup(response.text, 'lxml')
+                title = soup.find('div', {'class': 'product_main'}).find('h1')      #récupère les titres des livres
+                data_table = soup.find('table', {'class': 'table table-striped'})   #identifie la classe du tableau
+                data_table_ths = data_table('th')                                   #crée une variable avec les infos de th
+                data_table_tds = data_table.find_all('td')                          #crée une variable avec les infos de td
+                print('Titre: ' + title.text)                                       #écris dans le terminal "titre: + les infos des titres de livre
 
+                ths = []
 
-ths = []
+                for i, th in enumerate(data_table_ths):                             #itère les informations du tableau th
+                    ths.append(th.text)                                             #les ajoutes à la liste th
 
-for i, th in enumerate(data_table_ths):
-    ths.append(th.text)
+                tds = []
 
-tds = []
+                for i, td in enumerate(data_table_tds):
+                    tds.append(td.text)
 
-for i, td in enumerate(data_table_tds):
-    tds.append(td.text)
+                df_productInformation = pd.DataFrame(data={'intitulé': ths, 'informations': tds})
+                print(df_productInformation)
 
-df_productInformation = pd.DataFrame(data={'intitulé': ths, 'informations': tds})
-print(df_productInformation)
-
-df_productInformation.to_csv('./output.csv', encoding='utf-8', index=False)
+                #outf.write(title.text + ',' + th.text + ',' + td.text + '\n')
+df_productInformation.to_csv('./product_information.csv', encoding='utf-8', index=False)
