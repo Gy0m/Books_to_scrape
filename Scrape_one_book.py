7e5faf62d60eb65a119778777c5dc93bd996e37e
Gy0m <landreaug@gmail.com>
2021-03-22 15:04:54 +0100

    iteration titres

--- Scrape_one_book.py
+++ Scrape_one_book.py
@@ -3,7 +3,7 @@ import ipdb
 import pandas as pd
 from bs4 import BeautifulSoup
 
-'''
+
 
 url = 'http://books.toscrape.com/index.html'
 
@@ -25,23 +25,16 @@ with open('urls.csv', 'w') as file:
 
 with open('urls.csv', 'r') as file:
     for row in file:
+
         url = row.strip()
         response = requests.get(url)
         if response.ok:
             soup = BeautifulSoup(response.text, 'lxml')
-            upc = soup.find()
-            
-        '''
-
-url = 'http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html'
-response = requests.get(url)
-if response.ok:
-    soup = BeautifulSoup(response.text, 'lxml')
-    title = soup.find('div', {'class': 'product_main'}).find('h1')
-    data_table = soup.find('table', {'class': 'table table-striped'})
-    data_table_ths =data_table('th')
-    data_table_tds = data_table.find_all('td')
-    print('Titre: ' + title.text )
+            title = soup.find('div', {'class': 'product_main'}).find('h1')
+            data_table = soup.find('table', {'class': 'table table-striped'})
+            data_table_ths =data_table('th')
+            data_table_tds = data_table.find_all('td')
+            print('Titre: ' + title.text )
 
 
 ths = []
