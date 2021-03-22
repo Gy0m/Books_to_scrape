95eec9ba5fa2fc1a3744847536f5aedaf4a81eac
Gy0m <landreaug@gmail.com>
2021-03-22 23:48:29 +0100

    add installation instructions in readme

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
 # Books_to_scrap
 
 A little scrip to scrape imformations about books in the website 'http://books.toscrape.com/index.html'
+
+# Installation
+
+Clone the repo then :
+
+```
+cd Books-to-scrape 
+```
+then :
+
+```
+pip install -r requirements.txt
+```
+After you enter that command in the terminal :
+
+```
+python3 Scrape_one_book.py  
+```
+
+And that's it :)
--- Scrape_one_book.py
+++ Scrape_one_book.py
@@ -14,32 +14,32 @@ if response.ok:
     for div in divs:
         a = div.find('a')
         link = a['href']
-        links.append('http://books.toscrape.com/' + link)  #récupère les liens des images
-    print(len(links))              #compte le nombre de liens
+        links.append('http://books.toscrape.com/' + link)  # récupère les liens des images
+    print(len(links))              # compte le nombre de liens
 
 with open('urls.csv', 'w') as file:
     for link in links:
-        file.write(link + '\n')     #copie les liens dans un fichier csv et retour à la ligne
+        file.write(link + '\n')     # copie les liens dans un fichier csv et retour à la ligne
 
 with open('urls.csv', 'r') as file:
     with open('product_information.csv', 'w') as outf:
-        outf.write('intitulé,informations\n')
+        outf.write('intitulé, informations\n')
         for row in file:
 
             url = row.strip()
             response = requests.get(url)
             if response.ok:
                 soup = BeautifulSoup(response.text, 'lxml')
-                title = soup.find('div', {'class': 'product_main'}).find('h1')      #récupère les titres des livres
-                data_table = soup.find('table', {'class': 'table table-striped'})   #identifie la classe du tableau
-                data_table_ths = data_table('th')                                   #crée une variable avec les infos de th
-                data_table_tds = data_table.find_all('td')                          #crée une variable avec les infos de td
-                print('Titre: ' + title.text)                                       #écris dans le terminal "titre: + les infos des titres de livre
+                title = soup.find('div', {'class': 'product_main'}).find('h1')      # récupère les titres des livres
+                data_table = soup.find('table', {'class': 'table table-striped'})   # identifie la classe du tableau
+                data_table_ths = data_table('th')                              # crée une variable avec les infos de th
+                data_table_tds = data_table.find_all('td')                     # crée une variable avec les infos de td
+                print('Titre: ' + title.text)          # écris dans le terminal "titre: + les infos des titres de livre"
 
                 ths = []
 
-                for i, th in enumerate(data_table_ths):                             #itère les informations du tableau th
-                    ths.append(th.text)                                             #les ajoutes à la liste th
+                for i, th in enumerate(data_table_ths):                           # itère les informations du tableau th
+                    ths.append(th.text)                                           # les ajoutes à la liste th
 
                 tds = []
 
@@ -49,5 +49,5 @@ with open('urls.csv', 'r') as file:
                 df_productInformation = pd.DataFrame(data={'intitulé': ths, 'informations': tds})
                 print(df_productInformation)
 
-                #outf.write(title.text + ',' + th.text + ',' + td.text + '\n')
+                # outf.write(title.text + ',' + th.text + ',' + td.text + '\n')
 df_productInformation.to_csv('./product_information.csv', encoding='utf-8', index=False)
